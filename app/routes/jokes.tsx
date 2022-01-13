dac259b56273d3716c5ebdb801748424395a97db
Mohammadreza shad <m.shadmobaraki@gmail.com>
2022-01-14 00:18:25 +0330

    add : optimistic ui

--- app/routes/jokes.tsx
+++ app/routes/jokes.tsx
@@ -1,8 +1,14 @@
-import { Joke } from "@prisma/client";
-import { LinksFunction, LoaderFunction, useLoaderData } from "remix";
-import { Outlet, Link } from "remix";
-import stylesUrl from "~/styles/jokes.css";
+import { User } from "@prisma/client";
+import {
+  Link,
+  LinksFunction,
+  LoaderFunction,
+  Outlet,
+  useLoaderData,
+} from "remix";
 import { db } from "~/utils/db.server";
+import { getUser } from "~/utils/session.server";
+import stylesUrl from "../styles/jokes.css";
 
 export const links: LinksFunction = () => {
   return [
@@ -14,21 +20,28 @@ export const links: LinksFunction = () => {
 };
 
 type LoaderData = {
+  user: User | null;
   jokeListItems: Array<{ id: string; name: string }>;
 };
 
-export const loader: LoaderFunction = async () => {
+export const loader: LoaderFunction = async ({ request }) => {
+  const jokeListItems = await db.joke.findMany({
+    take: 5,
+    orderBy: { createdAt: "desc" },
+    select: { id: true, name: true },
+  });
+  const user = await getUser(request);
+
   const data: LoaderData = {
-    jokeListItems: await db.joke.findMany({
-      take: 5,
-      select: { id: true, name: true },
-      orderBy: { createdAt: "desc" },
-    }),
+    jokeListItems,
+    user,
   };
   return data;
 };
+
 export default function JokesRoute() {
   const data = useLoaderData<LoaderData>();
+
   return (
     <div className="jokes-layout">
       <header className="jokes-header">
@@ -39,6 +52,18 @@ export default function JokesRoute() {
               <span className="logo-medium">J🤪KES</span>
             </Link>
           </h1>
+          {data.user ? (
+            <div className="user-info">
+              <span>{`Hi ${data.user.username}`}</span>
+              <form action="/logout" method="post">
+                <button type="submit" className="button">
+                  Logout
+                </button>
+              </form>
+            </div>
+          ) : (
+            <Link to="/login">Login</Link>
+          )}
         </div>
       </header>
       <main className="jokes-main">
@@ -47,13 +72,11 @@ export default function JokesRoute() {
             <Link to=".">Get a random joke</Link>
             <p>Here are a few more jokes to check out:</p>
             <ul>
-              {data.jokeListItems.map((joke) => {
-                return (
-                  <li key={joke.id}>
-                    <Link to={joke.id}>{joke.name}</Link>
-                  </li>
-                );
-              })}
+              {data.jokeListItems.map((joke) => (
+                <li key={joke.id}>
+                  <Link to={joke.id}>{joke.name}</Link>
+                </li>
+              ))}
             </ul>
             <Link to="new" className="button">
               Add your own
--- app/routes/jokes/new.tsx
+++ app/routes/jokes/new.tsx
@@ -1,15 +1,33 @@
-import type { ActionFunction } from "remix";
-import { useActionData, redirect, json, Form } from "remix";
+import type { ActionFunction, LoaderFunction } from "remix";
+import {
+  useActionData,
+  redirect,
+  json,
+  useCatch,
+  Link,
+  Form,
+  useTransition,
+} from "remix";
+import { JokeDisplay } from "~/components/joke";
 import { db } from "~/utils/db.server";
+import { requireUserId, getUserId } from "~/utils/session.server";
 
-function validateJokeContent(content?: string) {
-  if (!content || content.length < 10) {
+export const loader: LoaderFunction = async ({ request }) => {
+  const userId = await getUserId(request);
+  if (!userId) {
+    throw new Response("Unauthorized", { status: 401 });
+  }
+  return {};
+};
+
+function validateJokeContent(content: string) {
+  if (content.length < 10) {
     return `That joke is too short`;
   }
 }
 
-function validateJokeName(name?: string) {
-  if (!name || name.length < 2) {
+function validateJokeName(name: string) {
+  if (name.length < 2) {
     return `That joke's name is too short`;
   }
 }
@@ -29,10 +47,10 @@ type ActionData = {
 const badRequest = (data: ActionData) => json(data, { status: 400 });
 
 export const action: ActionFunction = async ({ request }) => {
+  const userId = await requireUserId(request);
   const form = await request.formData();
   const name = form.get("name");
   const content = form.get("content");
-
   if (typeof name !== "string" || typeof content !== "string") {
     return badRequest({
       formError: `Form not submitted correctly.`,
@@ -48,12 +66,34 @@ export const action: ActionFunction = async ({ request }) => {
     return badRequest({ fieldErrors, fields });
   }
 
-  const joke = await db.joke.create({ data: fields });
+  const joke = await db.joke.create({
+    data: { ...fields, jokesterId: userId },
+  });
   return redirect(`/jokes/${joke.id}`);
 };
 
 export default function NewJokeRoute() {
   const actionData = useActionData<ActionData>();
+  const transition = useTransition();
+
+  if (transition.submission) {
+    const name = transition.submission.formData.get("name");
+    const content = transition.submission.formData.get("content");
+    if (
+      typeof name === "string" &&
+      typeof content === "string" &&
+      !validateJokeContent(content) &&
+      !validateJokeName(name)
+    ) {
+      return (
+        <JokeDisplay
+          joke={{ name, content }}
+          isOwner={true}
+          canDelete={false}
+        />
+      );
+    }
+  }
 
   return (
     <div>
@@ -72,7 +112,6 @@ export default function NewJokeRoute() {
               }
             />
           </label>
-
           {actionData?.fieldErrors?.name ? (
             <p className="form-validation-error" role="alert" id="name-error">
               {actionData.fieldErrors.name}
@@ -109,6 +148,29 @@ export default function NewJokeRoute() {
           </button>
         </div>
       </Form>
+    </div>
+  );
+}
+
+export function CatchBoundary() {
+  const caught = useCatch();
+
+  if (caught.status === 401) {
+    return (
+      <div className="error-container">
+        <p>You must be logged in to create a joke.</p>
+        <Link to="/login">Login</Link>
+      </div>
+    );
+  }
+}
+
+export function ErrorBoundary({ error }: { error: Error }) {
+  console.error(error);
+
+  return (
+    <div className="error-container">
+      Something unexpected went wrong. Sorry about that.
     </div>
   );
 }
